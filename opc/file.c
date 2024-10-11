10b0dfa7967f85cd7af42456910e4afa0b9d7be6
Keisuke Miyako <keisuke.miyako@4D.com>
2024-10-11 20:38:41 +0900

    Update file.c

--- opc/file.c
+++ opc/file.c
@@ -35,13 +35,70 @@
 #include <libxml/globals.h>
 #include <plib/plib.h>
 
+#if defined(WIN32)
+#include <windows.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#endif
+#ifndef struct_stat
+#  define struct_stat struct stat
+#endif
+
+/*
+ * patch to use utf-8 paths on windows instead of ANSI
+ *
+ */
+static FILE *ufopen(const char *filename, const char *mode)
+{
+#ifdef _WIN32
+    wchar_t    buf[_MAX_PATH];
+    wchar_t    _wfmode[99];    //should be enough
+    if(MultiByteToWideChar(CP_UTF8, 0, mode, -1, (LPWSTR)_wfmode, 99))
+    {
+        if(MultiByteToWideChar(CP_UTF8, 0, filename, -1, (LPWSTR)buf, _MAX_PATH))
+        {
+            return _wfopen((const wchar_t *)buf, (const wchar_t *)_wfmode);
+        }
+    }
+    return  fopen(filename, mode);
+#else
+    return fopen(filename, mode);
+#endif
+}
+
+static int uopen(const char *filename, int mode)
+{
+#ifdef _WIN32
+    wchar_t    buf[_MAX_PATH];
+    if(MultiByteToWideChar(CP_UTF8, 0, filename, -1, (LPWSTR)buf, _MAX_PATH))
+    {
+        return _wopen((const wchar_t *)buf, mode);
+    }
+    return 0;
+#else
+    return open(filename, mode);
+#endif
+}
+#ifdef _WIN32
+static int ustati64(const char *path, struct _stati64 *buffer)
+{
+
+    wchar_t    buf[_MAX_PATH];
+    if(MultiByteToWideChar(CP_UTF8, 0, path, -1, (LPWSTR)buf, _MAX_PATH))
+    {
+        return _wstati64((const wchar_t *)buf, buffer);
+    }
+    return -1;
+}
+#endif
+
 static void *opcFileOpen(const xmlChar *filename, int flags) {
     char mode[5];
     int mode_ofs=0;
     mode[mode_ofs++]='r';
     mode[mode_ofs++]='b';
     mode[mode_ofs++]='\0';
-    FILE *file=fopen((const char*)filename, mode); // try to open in READ mode...
+    FILE *file=ufopen((const char*)filename, mode); // try to open in READ mode...
     if (flags & OPC_FILE_WRITE) {
         if (NULL!=file && flags & OPC_FILE_TRUNC) {
             fclose(file); file=NULL; // force creating of new file..
@@ -53,7 +110,7 @@ static void *opcFileOpen(const xmlChar *filename, int flags) {
             mode[mode_ofs++]='+';
             mode[mode_ofs++]='b';
             mode[mode_ofs++]='\0';
-            file=fopen((const char *)filename, mode); // try to open new file
+            file=ufopen((const char *)filename, mode); // try to open new file
         } else {
             fclose(file); // close the read handle...
             mode_ofs=0;
@@ -61,7 +118,7 @@ static void *opcFileOpen(const xmlChar *filename, int flags) {
             mode[mode_ofs++]='+';
             mode[mode_ofs++]='b';
             mode[mode_ofs++]='\0';
-            file=fopen((const char *)filename, mode); // try to open existing for read/write
+            file=ufopen((const char *)filename, mode); // try to open existing for read/write
         }
     }
     return file;
