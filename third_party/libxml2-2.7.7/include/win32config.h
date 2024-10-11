5f3f33b004769ef084e0bf8fc9f977261693a8f2
Keisuke Miyako <keisuke.miyako@4d.com>
2024-10-11 20:52:42 +0900

    build

--- third_party/libxml2-2.7.7/include/win32config.h
+++ third_party/libxml2-2.7.7/include/win32config.h
@@ -95,7 +95,7 @@ static int isnan (double d) {
 
 #if defined(_MSC_VER)
 #define mkdir(p,m) _mkdir(p)
-#define snprintf _snprintf
+//#define snprintf _snprintf
 #if _MSC_VER < 1500
 #define vsnprintf(b,c,f,a) _vsnprintf(b,c,f,a)
 #endif
